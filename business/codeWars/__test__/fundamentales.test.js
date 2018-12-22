180f2da20db4abd0d22ae7765d54ecef2fd7f9cc
BertrandD <bernaz@pm.me>
2018-12-22 13:28:13 +0100

    add tribonnaci code war kata

---
+++ business/codeWars/__test__/fundamentales.test.js
@@ -0,0 +1,91 @@
+import { getMiddleCharacter, tribonacci } from "../fundamentals";
+
+describe("getMiddleCharacter", () => {
+  test("odd length", () => {
+    let string = "lol";
+    let expectedResult = "o";
+    expect(getMiddleCharacter(string)).toEqual(expectedResult);
+  });
+  test("even length", () => {
+    let string = "middle";
+    let expectedResult = "dd";
+    expect(getMiddleCharacter(string)).toEqual(expectedResult);
+  });
+  test("even length -test ", () => {
+    let string = "test";
+    let expectedResult = "es";
+    expect(getMiddleCharacter(string)).toEqual(expectedResult);
+  });
+});
+
+describe("tribonnaci", () => {
+  test("first case", () => {
+    let n = 10;
+    let signature = [1, 1, 1];
+    let expectedResult = [1, 1, 1, 3, 5, 9, 17, 31, 57, 105];
+    expect(tribonacci(signature, n)).toEqual(expectedResult);
+  });
+
+  test("second case", () => {
+    let n = 10;
+    let signature = [0, 0, 1];
+    let expectedResult = [0, 0, 1, 1, 2, 4, 7, 13, 24, 44];
+    expect(tribonacci(signature, n)).toEqual(expectedResult);
+  });
+  test("third case", () => {
+    let n = 0;
+    let signature = [300, 200, 100];
+    let expectedResult = [];
+    expect(tribonacci(signature, n)).toEqual(expectedResult);
+  });
+  test("fourth case", () => {
+    let n = 30;
+    let signature = [0.5, 0.5, 0.5];
+    let expectedResult = [
+      0.5,
+      0.5,
+      0.5,
+      1.5,
+      2.5,
+      4.5,
+      8.5,
+      15.5,
+      28.5,
+      52.5,
+      96.5,
+      177.5,
+      326.5,
+      600.5,
+      1104.5,
+      2031.5,
+      3736.5,
+      6872.5,
+      12640.5,
+      23249.5,
+      42762.5,
+      78652.5,
+      144664.5,
+      266079.5,
+      489396.5,
+      900140.5,
+      1655616.5,
+      3045153.5,
+      5600910.5,
+      10301680.5
+    ];
+    expect(tribonacci(signature, n)).toEqual(expectedResult);
+  });
+  test("fifth case", () => {
+    let n = 1;
+    let signature = [1, 1, 1];
+    let expectedResult = [1];
+    expect(tribonacci(signature, n)).toEqual(expectedResult);
+  });
+
+  test("sixth case", () => {
+    let n = 3;
+    let signature = [17, 0, 10];
+    let expectedResult = [17, 0, 10];
+    expect(tribonacci(signature, n)).toEqual(expectedResult);
+  });
+});
---
+++ business/codeWars/fundamentals.js
@@ -0,0 +1,30 @@
+export const getMiddleCharacter = string => {
+  const charArray = string.split("");
+  if (charArray.length === 1) {
+    return string;
+  }
+  const middleLenght = charArray.length / 2;
+  const flooredLetter = Math.floor(middleLenght);
+  let middleString = "";
+  if (charArray.length % 2 === 0) {
+    middleString = middleString
+      .concat(charArray[flooredLetter - 1])
+      .concat(charArray[flooredLetter]);
+  } else {
+    middleString = middleString.concat(charArray[flooredLetter]);
+  }
+  return middleString;
+};
+
+export const tribonacci = (signature, n) => {
+  if (n === 0) return [];
+  if (signature.length > n) return signature.slice(0, n);
+  const suite = signature;
+  if (suite.length < n) {
+    const next = suite.slice(-3).reduce((a, b) => a + b);
+    suite.push(next);
+    tribonacci(suite, n);
+  }
+
+  return suite;
+};
